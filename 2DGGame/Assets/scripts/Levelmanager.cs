c90a5a2a5fd301e7024554192eadd8eba24c026b
ououopel0001 <71679877+ououopel0001@users.noreply.github.com>
2020-11-10 15:58:46 +0800

    1110

---
+++ 2DGGame/Assets/scripts/Levelmanager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Levelmanager : MonoBehaviour
+{
+
+    public void NextLevel(string nameLv)
+    {
+        SceneManager.LoadScene(nameLv);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("選單");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
+
+
